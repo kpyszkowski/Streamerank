2e1eee4448a37716616d5eee3ebf40d7663aa39b
Kamil Pyszkowski <kamilpyszkowski@gmail.com>
2023-06-26 20:26:27 +0200

    Api: added Streamer model

---
+++ api/src/models/streamer.model.ts
@@ -0,0 +1,47 @@
+import { Schema, model } from 'mongoose'
+
+const streamerSchema = new Schema({
+  fullName: {
+    type: String,
+    maxLength: 72,
+    required: true,
+  },
+  nickName: {
+    type: String,
+    maxLength: 48,
+    required: true,
+  },
+  platform: {
+    type: String,
+    enum: ['twitch', 'youtube', 'tiktok', 'kick', 'rumble'],
+    required: true,
+  },
+  streamingUrl: {
+    type: String,
+    maxLength: 128,
+    required: true,
+  },
+  facebookUrl: {
+    type: String,
+    maxLength: 128,
+  },
+  instagramUrl: {
+    type: String,
+    maxLength: 128,
+  },
+  description: {
+    type: String,
+    maxLength: 512,
+    required: true,
+  },
+  upVotes: {
+    type: Number,
+    default: 0,
+  },
+  downVotes: {
+    type: Number,
+    default: 0,
+  },
+})
+
+export const ConversationModel = model('Streamer', streamerSchema)
